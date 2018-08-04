24028aedd54efb1b23401f98f42c2f7c291d89fc
avinashgazula <42089091+avinashgazula@users.noreply.github.com>
2018-08-04 17:51:17 +0530

    Add files via upload

---
+++ Thread Demo Programs/UserDaemonThreadDemo/UserDaemonThreadDemo.java
@@ -0,0 +1,34 @@
+// UserDaemonThreadDemo.java
+
+class UserDaemonThreadDemo
+{
+   public static void main (String [] args)
+   {
+      if (args.length == 0)
+         new MyThread ().start ();
+      else
+      {
+         MyThread mt = new MyThread ();
+         mt.setDaemon (true);
+         mt.start ();
+      }
+
+      try
+      {
+         Thread.sleep (1000);
+      }
+      catch (InterruptedException e)
+      {
+      }
+   }
+}
+
+class MyThread extends Thread
+{
+   public void run ()
+   {
+      System.out.println ("Daemon is " + isDaemon ());
+
+      while (true);
+   }
+}
